c377de18cf5ff561517520c06e835c4c342a9ea5
rdamala <ravi-damala@idexx.com>
2018-11-28 16:34:42 -0500

    defect fix

--- demofunction.js
+++ demofunction.js
@@ -13,4 +13,18 @@ var greetMe = function (){
     console.log("Hi This is Ravi");
 }
 testFun(greetMe);
+//##########################################################
+function test1(){
+var A = 3;
+var B = 6;
+res = A+B;
+console.log(res);
+}
+test1();
+test1();
 
+function testFirst(demo){
+    demo();
+    //return demo();
+}
+testFirst(test1);
