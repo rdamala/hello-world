1161257e129f305c03e1f6ae1d43489c4badf93e
rdamala <ravi-damala@idexx.com>
2021-03-15 21:52:27 -0400

    added new add.js and updated test1.s files

---
+++ add.js
@@ -0,0 +1,4 @@
+var a=5;
+var b = 12
+var c = a+b;
+console.log("sum is" +c);
--- test1.js
+++ test1.js
@@ -1,3 +1,5 @@
 console.log("This is new file");
 
 console.log("This is 2nd file for git testcommit");
+
+console.log("This is 3rd file for git testcommit for story2");
